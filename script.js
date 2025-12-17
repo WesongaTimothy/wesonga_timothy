c2a4c3930a227a7b60379b04b32ef2a3e7843793
Timothy Wesonga <107403615+WesongaTimothy@users.noreply.github.com>
2025-12-17 14:15:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -292,11 +292,31 @@ document.addEventListener('DOMContentLoaded', () => {
     // Change slide every 3 seconds
     setInterval(showNextSlide, 3000);
   </script>
+
+        <script>
+  const scrollBtn = document.getElementById("scrollTopBtn");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      scrollBtn.style.display = "block";
+    } else {
+      scrollBtn.style.display = "none";
+    }
+  });
+
+  scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+</script>
            });
             });
         });
     }
 });
+
 
 
 
